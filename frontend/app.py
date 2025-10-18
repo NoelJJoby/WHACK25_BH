9c67f0003f3f09f26996ccb6a252f861305cbe81
DMaxEA <edwinaning2007@gmail.com>
2025-10-18 17:59:07 +0100

    index changes

--- frontend/app.py
+++ frontend/app.py
@@ -5,8 +5,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
+def index():
+    return render_template("template.html")
+
+@app.route("/index")
 def home():
     return render_template("index.html")
  
